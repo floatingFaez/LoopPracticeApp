d88ae88b7f81e345f0dc127971de5039ae613c88
floatingFaez <floatingfaez@live.com>
2014-09-29 13:03:25 +0600

    Practice File Initially Uploaded

---
+++ loop/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string msg = "Looping Serize 1 to 10";
+
+            Console.WriteLine(msg);
+            for (int counter = 0; counter < 10; counter++ )
+            {
+                int i = counter+1;
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
